d5c11ce8bc0c24dfa58035096f4d2d40439b05eb
Joanice Oliveira <jhoaniceoliveira@gmail.com>
2022-04-19 14:19:02 -0300

    Adicionado desafio n°01 dos intermediários

--- src/main/java/br/gftwoman/intermediarios/MediaDois.java
+++ src/main/java/br/gftwoman/intermediarios/MediaDois.java
@@ -1,4 +1,31 @@
 package br.gftwoman.intermediarios;
 
+import java.util.Scanner;
+
 public class MediaDois {
+    public static void main(String[] args) {
+        Scanner dadosEntrada = new Scanner(System.in);
+        System.out.println("Digite a nota A: ");
+        double notaA = dadosEntrada.nextDouble();
+
+        System.out.println("Digite a nota B: ");
+        double notaB = dadosEntrada.nextDouble();
+
+        System.out.println("Digite a nota C: ");
+        double notaC = dadosEntrada.nextDouble();
+
+        int pesoA = 2;
+        int pesoB = 3;
+        int pesoC = 5;
+
+        int somaPesos = pesoA + pesoB + pesoC;
+
+        double mediaParcialA = notaA * pesoA;
+        double mediaParcialB = notaB * pesoB;
+        double mediaParcialC = notaC * pesoC;
+
+        double mediaFinal = (mediaParcialA + mediaParcialB + mediaParcialC) / somaPesos;
+        System.out.printf("MEDIA = %.1f", mediaFinal);
+
+    }
 }
